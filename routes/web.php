f7128d4b7d8bfc0f8786bbab20bea1562af65be0
Havind Ali <havind@ibyte.dev>
2022-06-24 12:44:20 +0300

    v-2

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/comments/{comment}/edit', function (Comment $comment) {
+    return view('comments.edit', ['comment' => $comment]);
+});
+
+Route::patch('/comments/{comment}', function (Comment $comment) {
+    $comment->update(
+        request()->validate(['body' => 'required|string'])
+    );
+    return redirect('/comments/$comment->id/edit');
+});
